b9265cb657f85911b28fc12fcbe486b4d1bce3ca
jalarcon <jalarcon@acsicorp.com>
2022-03-09 19:56:27 -0600

    Add update and delete functions to common_queries and repository

--- fastproject/modules/users/repository.py
+++ fastproject/modules/users/repository.py
@@ -85,3 +85,84 @@ async def get_user_by_id(
     if searched:
         return PublicUser(user_id=searched["uuser_id"], **searched)
     return None
+
+
+@with_connection
+async def update_user_by_id(
+    conn: PoolAcquireContext,
+    user_id: UUID,
+    username: Optional[str] = None,
+    email: Optional[str] = None,
+    first_name: Optional[str] = None,
+    last_name: Optional[str] = None,
+    password: Optional[str] = None,
+    is_superuser: Optional[bool] = None,
+    is_staff: Optional[bool] = None,
+    is_active: Optional[bool] = None,
+    date_joined: Optional[datetime.datetime] = None,
+    last_login: Optional[datetime.datetime] = None,
+) -> Optional[PublicUser]:
+    """
+    Updates the data of an existing user in the database with the given user_id
+    and returns it.
+
+    Not provided fields won't be updated
+
+    Args:
+      conn: A database connection.
+      user_id: The user_id of the user that will be updated.
+      username: The value for the "username" field of the user table.
+      email: The value for the "email" field of the user table.
+      first_name: The value for the "first_name" field of the user table.
+      last_name: The value for the "last_name" field of the user table.
+      password: The value for the "password" field of the user table.
+      is_superuser: The value for the "is_superuser" field of the user table.
+      is_staff: The value for the "is_staff" field of the user table.
+      is_active: The value for the "is_active" field of the user table.
+      date_joined: The value for the "date_joined" field of the user table.
+      last_login: The value for the "last_login" field of the user table.
+
+    Returns:
+      A PublicUser representing the updated user, None if the user was not
+      updated.
+
+    Raises:
+      UserUsernameAlreadyExistsError: If the username already exists.
+      UserEmailAlreadyExistsError: If the email already exists.
+    """
+    # TODO: test if exceptions can really happen.
+    try:
+        updated = await _queries.update_user_by_id(
+            conn, uuser_id=user_id, username=username, email=email,
+            first_name=first_name, last_name=last_name, password=password,
+            is_superuser=is_superuser, is_staff=is_staff, is_active=is_active,
+            date_joined=date_joined, last_login=last_login)
+        if updated:
+            return PublicUser(user_id=updated["user_id"], **updated)
+        return None
+    except asyncpg.exceptions.UniqueViolationError as e:
+        msg = str(e)
+        if "username" in msg:
+            raise UsernameAlreadyExistsError from e
+        if "email" in msg:
+            raise EmailAlreadyExistsError from e
+        raise e from e
+
+
+@with_connection
+async def delete_user_by_id(
+        conn: PoolAcquireContext, user_id: UUID) -> Optional[PublicUser]:
+    """Deletes a user from the database with the given user_id.
+
+    Args:
+      conn: A database connection.
+      user_id: The user_id of the user that will be deleted.
+
+    Returns:
+      A PublicUser representing the deleted user, None if the user was not
+      deleted.
+    """
+    deleted = await _queries.delete_user_by_id(conn, uuser_id=user_id)
+    if deleted:
+        return PublicUser(user_id=deleted["user_id"], **deleted)
+    return None
--- fastproject/modules/users/sql/common_queries.sql
+++ fastproject/modules/users/sql/common_queries.sql
@@ -1,9 +1,3 @@
--- name: get-all-users
--- Get all users
-SELECT *
-  FROM uuser;
-
-
 -- name: insert-user<!
 -- Insert a user
 INSERT INTO uuser (
@@ -28,18 +22,13 @@ INSERT INTO uuser (
     :is_active,
     :date_joined,
     :last_login
-) RETURNING
-    uuser_id,
-    username,
-    email,
-    first_name,
-    last_name,
-    password,
-    is_superuser,
-    is_staff,
-    is_active,
-    date_joined,
-    last_login;
+) RETURNING uuser.*;
+
+
+-- name: get-all-users
+-- Get all users
+SELECT *
+  FROM uuser;
 
 
 -- name: get-user-by-username^
@@ -50,36 +39,32 @@ SELECT *
 
 
 -- name: get-user-by-id^
--- Get a user with the given user_id
+-- Get a user with the given uuser_id
 SELECT *
   FROM uuser
  WHERE uuser_id = :uuser_id;
 
 
--- name: update-user-by-id
--- Update a user with the given user_id. The "coalesced" fields that were not
--- provided will be updated with their current value.
-UPDATE user
-   SET username = COALESCE(:username, username),
-       email = COALESCE(:email, email),
-       first_name = COALESCE(:first_name, first_name),
-       last_name = COALESCE(:last_name, last_name),
-       password = COALESCE(:password, password),
-       is_superuser = COALESCE(:is_superuser, is_superuser),
-       is_staff = COALESCE(:is_staff, is_staff),
-       is_active = COALESCE(:is_active, is_active),
-       date_joined = COALESCE(:date_joined, date_joined),
-       last_login = :last_login
- WHERE user_id = :user_id;
+-- name: update-user-by-id^
+-- Update a user with the given uuser_id. The "coalesced" fields that were not
+-- provided will be updated with their current value (their value won't change).
+   UPDATE uuser
+      SET username = COALESCE(:username, username),
+          email = COALESCE(:email, email),
+          first_name = COALESCE(:first_name, first_name),
+          last_name = COALESCE(:last_name, last_name),
+          password = COALESCE(:password, password),
+          is_superuser = COALESCE(:is_superuser, is_superuser),
+          is_staff = COALESCE(:is_staff, is_staff),
+          is_active = COALESCE(:is_active, is_active),
+          date_joined = COALESCE(:date_joined, date_joined),
+          last_login = :last_login
+    WHERE uuser_id = :uuser_id
+RETURNING uuser.*;
 
 
--- name: get-all-users-with-skill
--- Get all users that have the given skill
-SELECT uuser.first_name, skill.name
-  FROM uuser
-       JOIN uuser_skill
-       ON uuser_skill.uuser_id = uuser.uuser_id
-
-       LEFT JOIN skill
-       ON uuser_skill.skill_id = skill.skill_id
- WHERE skill.name IN ('Perseverance', 'Knowledge management', 'Giving clear feedback');
+-- name: delete-user-by-id^
+-- Delete a user with the given uuser_id
+DELETE FROM uuser
+      WHERE uuser_id = :uuser_id
+  RETURNING uuser.*;
